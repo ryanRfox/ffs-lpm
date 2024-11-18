87af73ac5d24cc97784db35ee5835d237cc60ccc
Ryan R. Fox <ryanRfox@users.noreply.github.com>
2024-11-18 09:47:11 -0500

    add http server

--- bot.py
+++ bot.py
@@ -9,6 +9,21 @@ from dotenv import load_dotenv
 from helpers.SimplePointsManager import PointsManagerSingleton
 from cogs import EXTENSIONS
 
+from aiohttp import web
+
+
+async def handler(request: web.Request) -> web.Response:
+    return web.Response(text="Hello world")
+
+
+async def init_app() -> web.Application:
+    app = web.Application()
+    app.add_routes([web.get("/", handler)])
+    return app
+
+
+web.run_app(init_app())
+
 intents = discord.Intents.default()
 
 
